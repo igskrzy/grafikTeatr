5c1875702b16fa89f2f858f02b2a8f2d55d8d5f6
igskrzy <skrzyniarz.ignacy@gmail.com>
2025-03-25 13:47:43 +0100

    Change int stanowisko to enum Stanowisko

--- main.cpp
+++ main.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
+enum Stanowisko { SZATNIARZ, PROGRAMOWY, BUFETOWY };
+
 struct Pracownik{
-    Pracownik(string imie, string nazwisko, int stanowisko, int il_minut) :  imie(imie), nazwisko(nazwisko), id(-1), stanowisko(stanowisko), il_minut(il_minut){}
+    Pracownik(string imie, string nazwisko, Stanowisko stanowisko, int il_minut) :  imie(imie), nazwisko(nazwisko), id(-1), stanowisko(stanowisko), il_minut(il_minut){}
     string imie;
     string nazwisko;
     int id;
-    int stanowisko; //0 - szatniarz, 1 - programowy, 2 - bufetowy
+    Stanowisko stanowisko;
     int il_minut;
 
     void wypisz(){
         cout << imie << " " << nazwisko << ", ";
-        if(stanowisko == 0) cout << "Szatniarz, ";
-        else if(stanowisko == 1) cout << "Programowy, ";
+        if(stanowisko == SZATNIARZ) cout << "Szatniarz, ";
+        else if(stanowisko == PROGRAMOWY) cout << "Programowy, ";
         else cout << "Bufetowy, ";
         cout << "Ilosc przepracowanych minut: " << il_minut << endl;
     }
@@ -186,32 +187,32 @@ struct Tydzien{
 };
 
 int main(){
-    Pracownik p1("Antoni", "Skrzyniarz", 1, 0);
-    Pracownik p2("Weronika", "Nagawiecka", 1, 450);
-    Pracownik p3("Szymon", "Kuczek", 1, 605);
-    Pracownik p4("Paulina", "Filimon", 1, 520);
-    Pracownik p5("Julia", "Pietron", 2, 480);
-    Pracownik p6("Aleksandra", "Sek", 2, 295);
-    Pracownik p7("Aleksandra", "Guzy", 2, 300);
-    Pracownik p8("Paulina", "Sokolowska", 2, 410);
-    Pracownik p9("Julia", "Ciembka", 2, 380);
-    Pracownik p10("Ignacy", "Skrzyniarz", 0, 390);
-    Pracownik p11("Olga", "Wiecek", 0, 470);
-    Pracownik p12("Iga", "Kozlowska", 0, 410);
-    Pracownik p13("Zofia", "Watroba", 0, 450);
-    Pracownik p14("Barbara", "Stojek", 0, 700);
-    Pracownik p15("Dominika", "Piatek", 0, 540);
-    Pracownik p16("Szczepan", "Lompe", 0, 290);
-    Pracownik p17("Katarzyna", "Pikul", 0, 290);
-    Pracownik p18("Laura", "Kamasz", 0, 370);
-    Pracownik p19("Lilianna", "Noga", 0, 320);
-    Pracownik p20("Celina", "Wojtanowska", 0, 370);
-    Pracownik p21("Jakub", "Drewnicki", 0, 270);
-    Pracownik p22("Julia", "Krawczyk", 0, 270);
-    Pracownik p23("Maja", "Duszak", 0, 410);
-    Pracownik p24("Patryk", "Kozlowski", 0, 350);
-    Pracownik p25("Tosia", "Malecka", 0, 550);
-    Pracownik p26("Zuzanna", "Adamowicz", 0, 280);
+    Pracownik p1("Antoni", "Skrzyniarz", PROGRAMOWY, 0);
+    Pracownik p2("Weronika", "Nagawiecka", PROGRAMOWY, 450);
+    Pracownik p3("Szymon", "Kuczek", PROGRAMOWY, 605);
+    Pracownik p4("Paulina", "Filimon", PROGRAMOWY, 520);
+    Pracownik p5("Julia", "Pietron", BUFETOWY, 480);
+    Pracownik p6("Aleksandra", "Sek", BUFETOWY, 295);
+    Pracownik p7("Aleksandra", "Guzy", BUFETOWY, 300);
+    Pracownik p8("Paulina", "Sokolowska", BUFETOWY, 410);
+    Pracownik p9("Julia", "Ciembka", BUFETOWY, 380);
+    Pracownik p10("Ignacy", "Skrzyniarz", SZATNIARZ, 390);
+    Pracownik p11("Olga", "Wiecek", SZATNIARZ, 470);
+    Pracownik p12("Iga", "Kozlowska", SZATNIARZ, 410);
+    Pracownik p13("Zofia", "Watroba", SZATNIARZ, 450);
+    Pracownik p14("Barbara", "Stojek", SZATNIARZ, 700);
+    Pracownik p15("Dominika", "Piatek", SZATNIARZ, 540);
+    Pracownik p16("Szczepan", "Lompe", SZATNIARZ, 290);
+    Pracownik p17("Katarzyna", "Pikul", SZATNIARZ, 290);
+    Pracownik p18("Laura", "Kamasz", SZATNIARZ, 370);
+    Pracownik p19("Lilianna", "Noga", SZATNIARZ, 320);
+    Pracownik p20("Celina", "Wojtanowska", SZATNIARZ, 370);
+    Pracownik p21("Jakub", "Drewnicki", SZATNIARZ, 270);
+    Pracownik p22("Julia", "Krawczyk", SZATNIARZ, 270);
+    Pracownik p23("Maja", "Duszak", SZATNIARZ, 410);
+    Pracownik p24("Patryk", "Kozlowski", SZATNIARZ, 350);
+    Pracownik p25("Tosia", "Malecka", SZATNIARZ, 550);
+    Pracownik p26("Zuzanna", "Adamowicz", SZATNIARZ, 280);
 
     Teatr teatrStary(26);
     teatrStary.dodaj_pracownika(&p1, 0);
